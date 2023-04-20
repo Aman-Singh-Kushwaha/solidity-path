055010080e6c53dcc5915705efd524c98ac50a5f
Aman-Singh-Kushwaha <amansinghkushwaha2003@gmail.com>
2023-04-20 18:45:02 +0530

    first contract

---
+++ basics/helloWorld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10; //Compiler version should be equal or greater to 0.8.10
+
+contract helloworld{
+    
+    /*  Syntax:
+      <dataType> <visibility> <varName> = <data>;
+    */
+    string public myString = "Hello World, Aman Here";
+
+}
