4c2c8777578eef346d98f081a2615a0013481bdf
Aman-Singh-Kushwaha <amansinghkushwaha2003@gmail.com>
2023-04-25 16:29:11 +0530

    learned looping

---
+++ basics/loops.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract forAndWhileLoops{   
+    uint public i=0;
+// loops may be forcefully terminated transaction cost reaches gas limit
+    function loops() external ) {
+        uint a;
+        for(uint j=0; j< 10; j++) {   //For loops
+            if(j==3) continue;
+            else if(j==5) break;
+
+            a++;
+        }
+
+        while(true){
+            if(a==100) break;  // on production never loop longer or pay higher gas
+            a++;
+        }
+        i=a;
+    }
+
+}
