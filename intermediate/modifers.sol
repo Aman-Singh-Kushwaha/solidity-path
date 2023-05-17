69069cde051410f13a31035433f41c53c0395034
Aman Kumar Singh Kushwaha <96011814+Aman-Singh-Kushwaha@users.noreply.github.com>
2023-05-17 07:14:53 +0000

    added parameterized modifier

--- intermediate/modifers.sol
+++ intermediate/modifers.sol
@@ -5,13 +5,20 @@ contract Modifier{
     //State Variables
     address public owner = msg.sender; // contract deployer address here
     string internal secretPassword = "123456789";
+    uint timesBeingCalled = 0;
 
     modifier isOwner{
         require(msg.sender==owner, "Access Denied, only owner can access"); //will return err message on modifier denial
         _;
     }
 
+    modifier callLimit( uint _callNumber){
+        require(_callNumber < 5, "Password Check Limit Exceeded");
+        _;
+    }
+
     function checkPassword() public view isOwner returns(string memory){
+        timesBeingCalled++;
         return secretPassword;
     }
 }
